2e6d9d65336f302acea004f139b672ced516eec1
Akshay Rathod <kr.akshay234@gmail.com>
2023-09-15 12:41:27 +0530

    created userSchema

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,49 +1,51 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: [true, "Please provide a first name"],
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "Please provide a first name"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Please provide a last name"],
+    },
+    age: {
+      type: Number,
+      required: [true, "Please provide an age"],
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: [true, "Please provide a phone number"],
+    },
+    address: {
+      type: String,
+      required: [true, "Please provide an address"],
+    },
+    city: {
+      type: String,
+      required: [true, "Please provide a city"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide an email address"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+    },
+    budgets: {
+      type: Array,
+      default: [],
+    },
   },
-  lastName: {
-    type: String,
-    required: [true, "Please provide a last name"],
-  },
-  age: {
-    type: Number,
-    required: [true, "Please provide an age"],
-  },
-  gender: {
-    type: String,
-    required: true,
-  },
-  phone: {
-    type: Number,
-    required: [true, "Please provide a phone number"],
-  },
-  address: {
-    type: String,
-    required: [true, "Please provide an address"],
-  },
-  city: {
-    type: String,
-    required: [true, "Please provide a city"],
-  },
-  email: {
-    type: String,
-    required: [true, "Please provide an email address"],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Please provide a password"],
-  },
-  budgets: {
-    type: Array,
-    default: [],
-  },
-  
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
